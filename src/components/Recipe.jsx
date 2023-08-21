22dc4936913807d6f347e4d1c069cf539df5fcd5
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2023-08-21 23:27:32 +0800

    components: update recipe card

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,26 +1,36 @@
 import propTypes from "prop-types";
+import { useState } from "react";
 
-export default function Recipe({title,image,calories,ingredients}){
+export default function Recipe({label,image,calories,ingredients,totalTime}){
+    let prepTime = 0;
+    if(totalTime > 60){
+       prepTime = totalTime/60;
+       prepTime = prepTime.toFixed(2) + " hr"
+    }
+    else{
+        prepTime = totalTime + " min";
+    }
     return(
         <>
-             <div>
-            <h1>{title}</h1>
-
-              
-<p>Calories : {calories}</p>
-  
-            <img src={image} alt=""/>
-  
-        </div>
+            <div className="mt-10" >
+                
+                <img src={image} alt={label} className="rounded object-cover h-56 w-96"/> 
+                <div className="flex flex-direction justify-between">
+                    <h1>{label}</h1>
+                    <small className="text-slate-600">{prepTime}</small>
+                </div>
+                <small className="text-slate-600">{ingredients.length} ingredients</small> 
+            </div>
         </>
     )
 
 }
 
 Recipe.propTypes = {
-    title: propTypes.string,
+    label: propTypes.string,
     image: propTypes.string,
     calories: propTypes.number,
-    ingredients: propTypes.array
+    ingredients: propTypes.array,
+    totalTime: propTypes.number
 }
 
