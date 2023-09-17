b4aefacf54fe95d21f168e43367998b32678c76d
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2023-09-17 22:05:44 +0800

    Navigation Bar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+export default function NavBar(){
+    return(
+        <>
+            <div className="h-[70px] bg-red-400 flex items-center p-2">
+                <h1 className="text-white font-semibold text-2xl">My Recipes</h1>
+            </div>
+        </>
+    )
+}
