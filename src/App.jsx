e690f2ba10cbb30ed39476d14f5ad0b9c5934624
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2023-09-17 22:07:22 +0800

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,11 @@
-import { useState,useEffect } from "react";
 import Recipe from './components/Recipe.jsx'
+import NavBar from "./components/NavBar.jsx";
 export default function App(){
-  const APP_KEY =  'ed7c1f438f1e6ee45363d8863aac483b'
-  const APP_ID = 'd52f0aa6'
-  const [recipes,setRecipe] = useState([]);
-  const [search,setSearch] = useState("");
-  const [query,setQuery] = useState('chicken');
-  //const [dataLength,setLength] = useState(0);
-  //const query = 'chicken'
-  useEffect(()=>{
-    fetchRecipes();
-  },[query]);
-  //get recipes
-  const fetchRecipes = async() => {
-    try{
-      const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
-      const jsonData = await response.json();
-      setRecipe(jsonData.hits);
-      //setLength(jsonData.length);
-      console.log(jsonData)
-    }
-    catch(error){
-      console.error('Error fetching data',error)
-    }
-    
-  }
-  const updateSearch = e => {
-    setSearch(e.target.value)
-  }
-  const submitForm = e =>{
-    e.preventDefault();
-    setQuery(search)
-    setSearch("")
-    //console.log(dataLength)
-  }
- 
   return(
     <>
-      <div className="p-3 grid place-items-center">
-        <form onSubmit={submitForm} className="flex flex-row ">
-          <input type="text" 
-            value={search} 
-            onChange={updateSearch} 
-            className="border-2 border-slate-400 mx-3 w-80"
-            placeholder="Enter recipe name"/>
-          <button 
-            type="submit" 
-            className="rounded-full bg-blue-500 text-white p-2 w-32">
-            Search
-          </button>
-        </form>
-        <div className="grid grid-rows-5 grid-flow-col gap-8">
-        {recipes.map((recipe,index) => (
-           
-           <Recipe
-           className=""
-           key={index}
-           title={recipe.recipe.title}
-           label={recipe.recipe.label}
-           calories={recipe.recipe.calories}
-           image={recipe.recipe.image}
-           ingredients={recipe.recipe.ingredients}
-           totalTime={recipe.recipe.totalTime}
-         />
-        ))}
-        </div>
-      </div>
+      <NavBar/>
+     
+      <Recipe/>
     </>
   )
 }
-
--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,36 +1,47 @@
-import propTypes from "prop-types";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import 'react-loading-skeleton/dist/skeleton.css'
+import Skeleton from "react-loading-skeleton";
+export default function Recipe(){
+    const APP_KEY =  'ed7c1f438f1e6ee45363d8863aac483b'
+    const APP_ID = 'd52f0aa6' 
+    const [recipes,setRecipes] = useState([]);
+    const [query,setQuery] = useState('salad');
+    const [searchRecipes,setSearchRecipes] = useState();
+    const [isLoading,setLoading] = useState(true);
+    useEffect(() => {
+        fetchRecipes();
+    },[query]);
 
-export default function Recipe({label,image,calories,ingredients,totalTime}){
-    let prepTime = 0;
-    if(totalTime > 60){
-       prepTime = totalTime/60;
-       prepTime = prepTime.toFixed(2) + " hr"
+    const fetchRecipes = async() => {
+        try{
+            const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+            const json = await response.json();
+            setLoading(false);
+            setRecipes(json.hits)
+            console.log(json)
+        }
+        catch(error){
+            console.log(error);
+        }
     }
-    else{
-        prepTime = totalTime + " min";
-    }
+    const handleChange = (e) => {setSearchRecipes(e.target.value)}
+    const btnSearch = () => {setQuery(searchRecipes)}
     return(
         <>
-            <div className="mt-10" >
-                
-                <img src={image} alt={label} className="rounded object-cover h-56 w-96"/> 
-                <div className="flex flex-direction justify-between">
-                    <h1>{label}</h1>
-                    <small className="text-slate-600">{prepTime}</small>
-                </div>
-                <small className="text-slate-600">{ingredients.length} ingredients</small> 
+            <div className="flex justify-center items-center h-40 gap-3">
+                <input type="text" placeholder="Enter Recipe" className="outline-none p-2 border-2 border-red-300/100" onChange={handleChange} />
+                <button className="w-36 bg-red-300 text-white p-2 rounded-full" onClick={btnSearch}>Search</button>
+            </div>
+            <div className="grid place-items-center grid-cols-1 md:grid-cols-3 gap-5">
+                {isLoading ?  ''  : recipes.map((item,index) => (
+                   <div  className="" key={index}>
+                       <img src={item.recipe.image} alt={item.recipe.image} />
+                       <p>{item.recipe.label}</p>
+                   </div>
+               ))}
             </div>
         </>
     )
 
-}
-
-Recipe.propTypes = {
-    label: propTypes.string,
-    image: propTypes.string,
-    calories: propTypes.number,
-    ingredients: propTypes.array,
-    totalTime: propTypes.number
 }
 
