818830f7618731da9a29f8316707e85d34962688
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2023-09-19 21:24:05 +0800

    feat: add react router for browser routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import Recipe from './components/Recipe.jsx'
 import NavBar from "./components/NavBar.jsx";
+import Ingredients from "./page/Ingredients.jsx"
+import { Routes,Route, BrowserRouter } from 'react-router-dom';
+
 export default function App(){
   return(
     <>
-      <NavBar/>
-     
-      <Recipe/>
+      <BrowserRouter>
+        <NavBar/>
+        <Routes>
+          <Route index element={<Recipe/>}/>
+          <Route path="/ingredients" element={<Ingredients/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
