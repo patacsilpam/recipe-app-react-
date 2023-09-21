ea411021532ee7a80064f2cdbb0f376ea72b8563
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2023-09-21 22:48:00 +0800

    feat: set a document title

--- src/page/Ingredients.jsx
+++ src/page/Ingredients.jsx
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 export default function Ingredients(){
+    const {id} = useParams();
+    useEffect(()=>{
+        document.title = `Foody | ${id}`;
+    })
     return(
         <>
-            Ingredients
+            <span>{id}sdf</span>
+
         </>
     )
 }
